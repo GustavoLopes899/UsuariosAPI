50024f5b80b993e71e6d3490456584ea3aed7a7b
Gustavo Lopes <GustavoLopes899@gmail.com>
2021-08-02 17:27:24 -0300

    Adiciona estrutura inicial dos projetos
    Foram adicionados as estruturas iniciais dos projetos que
    serão implementados.

---
+++ Backend/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("")]
+    [ApiController]
+    [Produces("application/json")]
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Página inicial da aplicação
+        /// </summary>
+        /// <returns></returns>
+        /// /// <response code="200">Retorna a mensagem da página inicial da API</response>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return this.Ok("Página inicial");
+        }
+    }
+}
