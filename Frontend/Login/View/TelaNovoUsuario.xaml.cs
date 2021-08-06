20293577fec55998eae7cbf750f47de7fb54a6bb
Gustavo Lopes <GustavoLopes899@gmail.com>
2021-08-06 10:11:21 -0300

    Adiciona estrutura inicial da tela de novo usuário
    Os arquivos da tela de novo usuário foi adicionados, porém
    ainda falta sua implementação.

---
+++ Frontend/Login/View/TelaNovoUsuario.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Frontend.Login.VM;
+using System.Windows;
+
+namespace Frontend.Login.View
+{
+    /// <summary>
+    /// Interaction logic for TelaNovoUsuario.xaml
+    /// </summary>
+    public partial class TelaNovoUsuario : Window
+    {
+        public TelaNovoUsuario()
+        {
+            this.InitializeComponent();
+            this.DataContext = new NovoUsuarioVM();
+        }
+    }
+}
