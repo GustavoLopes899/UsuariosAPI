315035b4f52518d1c41789185878164f220dad6a
Gustavo Lopes <GustavoLopes899@gmail.com>
2021-08-06 10:15:53 -0300

    Adiciona implementação da tela der login
    É possível agora autenticar o usuário utilizando a tela de login.

--- Frontend/Login/VM/LoginVM.cs
+++ Frontend/Login/VM/LoginVM.cs
@@ -1,8 +1,44 @@
-﻿namespace Frontend.Login.VM
+﻿using Ferramentas;
+using Frontend.Modelos;
+using Frontend.Program.Sessao;
+using Frontend.Propriedades.Util;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Windows;
+
+namespace Frontend.Login.VM
 {
-    // TODO: Implementar LoginVM
     public class LoginVM
     {
-
+        public bool Autentica(string usuario, string senha)
+        {
+            string senhaCriptografada = MD5Hash.CalculaHash(senha);
+            IDictionary<string, object> dados = new Dictionary<string, object>
+            {
+                { "usuario", usuario },
+                { "senha", senhaCriptografada }
+            };
+            try
+            {
+                string resposta = Requisicoes.Post(Sessao.UrlAutenticacao, dados, null);
+                if (!string.IsNullOrEmpty(resposta))
+                {
+                    TokenSessao token = JsonSerializer.Deserialize<TokenSessao>(resposta);
+                    if (token.Token != null)
+                    {
+                        Sessao.Token = token;
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show("Erro ao efetuar o login. Verifique o log", "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+                string mensagem = Logger.TrataMensagemExcecao(ex);
+                Logger.GravarLog(mensagem);
+            }
+            return false;
+        }
     }
 }
--- Frontend/Login/View/TelaLogin.xaml.cs
+++ Frontend/Login/View/TelaLogin.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Frontend.Login.VM;
+﻿using Frontend.JanelaPrincipal.View;
+using Frontend.JanelaPrincipal.VM;
+using Frontend.Login.VM;
 using System.Windows;
 
 namespace Frontend.Login.View
@@ -14,6 +16,39 @@ namespace Frontend.Login.View
             this.Height = altura;
             this.Width = largura;
             this.DataContext = new LoginVM();
+        }
+
+        private void FazerLogin(object sender, RoutedEventArgs e)
+        {
+            string usuario = this.tbUsuario?.Text;
+            string senha = this.pbSenha?.Password;
+
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(senha))
+            {
+                _ = MessageBox.Show("Usuário e/ou senha não podem estar em branco!", "Aviso!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (this.DataContext is LoginVM loginVM)
+            {
+                if (loginVM.Autentica(usuario, senha))
+                {
+                    TelaJanelaPrincipal janelaPrincipal = new TelaJanelaPrincipal
+                    {
+                        DataContext = new JanelaPrincipalVM()
+                    };
+                    janelaPrincipal.Show();
+                    this.Close();
+                }
+                else
+                {
+                    _ = MessageBox.Show("Login não autorizado! Usuário e/ou senha incorretos!", "Login inválido!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void AbrirJanelaNovoUsuario(object sender, RoutedEventArgs e)
+        {
+            TelaNovoUsuario novoUsuario = new TelaNovoUsuario();
+            _ = novoUsuario.ShowDialog();
         }
     }
 }
